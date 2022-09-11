f47f513349c4a868887ee9219b86a10a71400698
Arnorian58 <arnorian58@gmail.com>
2022-09-11 09:54:28 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-/**
- * main - prints the alphabet in lowercase,
- * followed by a new line
- * Return: Always 0 (success)
- */
-int main (void )
-{
-	char ch;
+ /**
+  * main - prints the alphabet in lowercase,
+  * followed by a new line
+  * Return: Always 0 (success)
+  */
+ int main (void )
+ {
+      	  char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
+	  for (ch = 'a'; ch <= 'z'; ch++)
+	  {
 
-		putchar(ch);
-	}
-	putchar('\n');
-	return (0);
+		  putchar(ch);
+	  }
+	  putchar('\n');
+	  return (0);
 }
